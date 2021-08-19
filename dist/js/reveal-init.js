75eb640b17cf2b9b5570d81326175560b465dc6c
Francisco Montenegro <franciscomontenegro22@gmail.com>
2021-08-19 07:49:18 -0300

    added some plugins

---
+++ dist/js/reveal-init.js
@@ -0,0 +1,20 @@
+Reveal.initialize({
+
+    /*Definicion de los Plugins*/ 
+    plugins: [ RevealMarkdown, RevealHighlight  ],
+
+    /* auto reproduccion del video*/    
+    autoPlayMedia: true,
+    /* numero de slide */
+    slideNumber: 'true',
+    /*AutoSlide*/
+    autoSlide: 93000,
+    loop: true,
+    /*    autoSlideMethod: Reveal.navigateRight,*/
+    /*transition por defecto para todos los slides*/
+    backgroundTransition: 'slide'
+  });
+
+  /*Options
+  Data Transition: [none, fade, zoom, slide, covex, concave]
+  */
---
+++ md/anuncio01.md
@@ -0,0 +1,15 @@
+﻿### Verificación de los Requisitos
+
+ - Certificado de Antecedentes Provincial (Certificado de Buena
+   Conducta): *****3 meses***** 
+  - Constancia de Antigüedad y Sanciones en el Poder
+   Judicial: ***6 meses***
+   
+ - Constancia de Inscripción en la Matricula: ***6 meses***
+   
+ - Registro de Deudores Alimentarios Morosos (REDAM): ***1 año*** 
+ - Constancia
+   de Aptitud Psicofísica: ***6 meses***
+  
+ - Domicilio Real 
+ - Integración Familiar
